816b2fe0baddbf48712c2763f75ee25b1185a24b
Badr_Annabi <badrannabi@gmail.com>
2023-12-28 15:56:29 +0100

    printf

---
+++ printf/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdrarg.h>
+#include <stdio.h>
+#include <unistd.h>
+#endif
+
+int _printf(const char *format, ...);
---
+++ printf/printf.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+/**
+ * _printf -  produces output according to a format.
+ * @format: is a character string.
+ * Return:  the number of characters printed
+ * (excluding the null byte used to end output to strings)
+*/
